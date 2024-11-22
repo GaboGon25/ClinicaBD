587be93058b614ffec9e7ba7a6cbf66d319a03ff
KevinMuno <kevindediosant@gmail.com>
2024-11-22 16:26:53 -0600

    Registro de Pacientes listo. Solo falta crud de ocupacio

--- frmPaciente.Designer.cs
+++ frmPaciente.Designer.cs
@@ -29,25 +29,28 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            dataGridView1 = new DataGridView();
-            button1 = new Button();
-            button2 = new Button();
+            dgvPacientes = new DataGridView();
+            btnAgregarP = new Button();
+            btnEditarP = new Button();
             pnlTitulo = new Panel();
             groupBox1 = new GroupBox();
+            txtCorreo = new TextBox();
+            label9 = new Label();
+            txtPacienteID = new TextBox();
             linkLabel1 = new LinkLabel();
-            textBox4 = new TextBox();
+            txtDireccion = new TextBox();
             label7 = new Label();
-            comboBox1 = new ComboBox();
+            cmbOcupacion = new ComboBox();
             label6 = new Label();
-            textBox3 = new TextBox();
+            txtCelular = new TextBox();
             label5 = new Label();
             label4 = new Label();
-            dateTimePicker1 = new DateTimePicker();
-            textBox2 = new TextBox();
+            dtpFechaNacimiento = new DateTimePicker();
+            txtApellidoP = new TextBox();
             label3 = new Label();
-            textBox1 = new TextBox();
+            txtNombreP = new TextBox();
             label2 = new Label();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvPacientes).BeginInit();
             pnlTitulo.SuspendLayout();
             groupBox1.SuspendLayout();
             SuspendLayout();
@@ -63,36 +66,38 @@
             label1.TabIndex = 0;
             label1.Text = "Registro de Paciente";
             // 
-            // dataGridView1
+            // dgvPacientes
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(113, 428);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(1245, 290);
-            dataGridView1.TabIndex = 13;
+            dgvPacientes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvPacientes.Location = new Point(113, 458);
+            dgvPacientes.Name = "dgvPacientes";
+            dgvPacientes.Size = new Size(1245, 290);
+            dgvPacientes.TabIndex = 13;
+            dgvPacientes.CellClick += dgvPacientes_CellClick;
             // 
-            // button1
+            // btnAgregarP
             // 
-            button1.BackColor = Color.DarkGreen;
-            button1.Font = new Font("Segoe UI", 15F);
-            button1.Location = new Point(557, 361);
-            button1.Name = "button1";
-            button1.Size = new Size(203, 61);
-            button1.TabIndex = 14;
-            button1.Text = "AGREGAR";
-            button1.UseVisualStyleBackColor = false;
+            btnAgregarP.BackColor = Color.DarkGreen;
+            btnAgregarP.Font = new Font("Segoe UI", 15F);
+            btnAgregarP.Location = new Point(556, 382);
+            btnAgregarP.Name = "btnAgregarP";
+            btnAgregarP.Size = new Size(203, 61);
+            btnAgregarP.TabIndex = 14;
+            btnAgregarP.Text = "AGREGAR";
+            btnAgregarP.UseVisualStyleBackColor = false;
+            btnAgregarP.Click += btnAgregarP_Click;
             // 
-            // button2
+            // btnEditarP
             // 
-            button2.BackColor = Color.DodgerBlue;
-            button2.Font = new Font("Segoe UI", 15F);
-            button2.Location = new Point(786, 361);
-            button2.Name = "button2";
-            button2.Size = new Size(203, 61);
-            button2.TabIndex = 15;
-            button2.Text = "EDITAR";
-            button2.UseVisualStyleBackColor = false;
-            button2.Click += button2_Click;
+            btnEditarP.BackColor = Color.DodgerBlue;
+            btnEditarP.Font = new Font("Segoe UI", 15F);
+            btnEditarP.Location = new Point(786, 382);
+            btnEditarP.Name = "btnEditarP";
+            btnEditarP.Size = new Size(203, 61);
+            btnEditarP.TabIndex = 15;
+            btnEditarP.Text = "EDITAR";
+            btnEditarP.UseVisualStyleBackColor = false;
+            btnEditarP.Click += btnEditarP_Click;
             // 
             // pnlTitulo
             // 
@@ -105,26 +110,54 @@
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(txtCorreo);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(txtPacienteID);
             groupBox1.Controls.Add(linkLabel1);
-            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(txtDireccion);
             groupBox1.Controls.Add(label7);
-            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(cmbOcupacion);
             groupBox1.Controls.Add(label6);
-            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(txtCelular);
             groupBox1.Controls.Add(label5);
             groupBox1.Controls.Add(label4);
-            groupBox1.Controls.Add(dateTimePicker1);
-            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(dtpFechaNacimiento);
+            groupBox1.Controls.Add(txtApellidoP);
             groupBox1.Controls.Add(label3);
-            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(txtNombreP);
             groupBox1.Controls.Add(label2);
             groupBox1.Font = new Font("Segoe UI", 12F);
             groupBox1.Location = new Point(38, 94);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(1320, 248);
+            groupBox1.Size = new Size(1320, 282);
             groupBox1.TabIndex = 17;
             groupBox1.TabStop = false;
             groupBox1.Text = "DATOS PERSONALES";
+            // 
+            // txtCorreo
+            // 
+            txtCorreo.Location = new Point(151, 228);
+            txtCorreo.Name = "txtCorreo";
+            txtCorreo.Size = new Size(236, 29);
+            txtCorreo.TabIndex = 29;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Verdana", 12F);
+            label9.Location = new Point(54, 233);
+            label9.Name = "label9";
+            label9.Size = new Size(61, 18);
+            label9.TabIndex = 28;
+            label9.Text = "Correo";
+            // 
+            // txtPacienteID
+            // 
+            txtPacienteID.Location = new Point(151, 28);
+            txtPacienteID.Name = "txtPacienteID";
+            txtPacienteID.Size = new Size(100, 29);
+            txtPacienteID.TabIndex = 26;
+            txtPacienteID.Visible = false;
             // 
             // linkLabel1
             // 
@@ -137,13 +170,13 @@
             linkLabel1.Text = "Agregar mas ocupaciones";
             linkLabel1.LinkClicked += linkLabel1_LinkClicked;
             // 
-            // textBox4
+            // txtDireccion
             // 
-            textBox4.Location = new Point(929, 129);
-            textBox4.Multiline = true;
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(323, 109);
-            textBox4.TabIndex = 24;
+            txtDireccion.Location = new Point(929, 129);
+            txtDireccion.Multiline = true;
+            txtDireccion.Name = "txtDireccion";
+            txtDireccion.Size = new Size(323, 109);
+            txtDireccion.TabIndex = 24;
             // 
             // label7
             // 
@@ -155,13 +188,13 @@
             label7.TabIndex = 23;
             label7.Text = "Direccion:";
             // 
-            // comboBox1
+            // cmbOcupacion
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(940, 77);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(163, 29);
-            comboBox1.TabIndex = 22;
+            cmbOcupacion.FormattingEnabled = true;
+            cmbOcupacion.Location = new Point(940, 77);
+            cmbOcupacion.Name = "cmbOcupacion";
+            cmbOcupacion.Size = new Size(163, 29);
+            cmbOcupacion.TabIndex = 22;
             // 
             // label6
             // 
@@ -173,12 +206,12 @@
             label6.TabIndex = 21;
             label6.Text = "Ocupacion:";
             // 
-            // textBox3
+            // txtCelular
             // 
-            textBox3.Location = new Point(577, 158);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(236, 29);
-            textBox3.TabIndex = 20;
+            txtCelular.Location = new Point(577, 158);
+            txtCelular.Name = "txtCelular";
+            txtCelular.Size = new Size(236, 29);
+            txtCelular.TabIndex = 20;
             // 
             // label5
             // 
@@ -200,19 +233,19 @@
             label4.TabIndex = 18;
             label4.Text = "Fecha de Nacimiento:";
             // 
-            // dateTimePicker1
+            // dtpFechaNacimiento
             // 
-            dateTimePicker1.Location = new Point(585, 77);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(236, 29);
-            dateTimePicker1.TabIndex = 17;
+            dtpFechaNacimiento.Location = new Point(585, 77);
+            dtpFechaNacimiento.Name = "dtpFechaNacimiento";
+            dtpFechaNacimiento.Size = new Size(236, 29);
+            dtpFechaNacimiento.TabIndex = 17;
             // 
-            // textBox2
+            // txtApellidoP
             // 
-            textBox2.Location = new Point(151, 158);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(236, 29);
-            textBox2.TabIndex = 16;
+            txtApellidoP.Location = new Point(151, 158);
+            txtApellidoP.Name = "txtApellidoP";
+            txtApellidoP.Size = new Size(236, 29);
+            txtApellidoP.TabIndex = 16;
             // 
             // label3
             // 
@@ -224,12 +257,12 @@
             label3.TabIndex = 15;
             label3.Text = "Apellido:";
             // 
-            // textBox1
+            // txtNombreP
             // 
-            textBox1.Location = new Point(151, 77);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(236, 29);
-            textBox1.TabIndex = 14;
+            txtNombreP.Location = new Point(151, 77);
+            txtNombreP.Name = "txtNombreP";
+            txtNombreP.Size = new Size(236, 29);
+            txtNombreP.TabIndex = 14;
             // 
             // label2
             // 
@@ -248,13 +281,14 @@
             ClientSize = new Size(1607, 769);
             Controls.Add(groupBox1);
             Controls.Add(pnlTitulo);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(dataGridView1);
+            Controls.Add(btnEditarP);
+            Controls.Add(btnAgregarP);
+            Controls.Add(dgvPacientes);
             FormBorderStyle = FormBorderStyle.None;
             Name = "frmPaciente";
             Text = "frmPaciente";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            Load += frmPaciente_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvPacientes).EndInit();
             pnlTitulo.ResumeLayout(false);
             pnlTitulo.PerformLayout();
             groupBox1.ResumeLayout(false);
@@ -265,23 +299,26 @@
         #endregion
 
         private Label label1;
-        private DataGridView dataGridView1;
-        private Button button1;
-        private Button button2;
+        private DataGridView dgvPacientes;
+        private Button btnAgregarP;
+        private Button btnEditarP;
         private Panel pnlTitulo;
         private GroupBox groupBox1;
-        private TextBox textBox4;
+        private TextBox txtDireccion;
         private Label label7;
-        private ComboBox comboBox1;
+        private ComboBox cmbOcupacion;
         private Label label6;
-        private TextBox textBox3;
+        private TextBox txtCelular;
         private Label label5;
         private Label label4;
-        private DateTimePicker dateTimePicker1;
-        private TextBox textBox2;
+        private DateTimePicker dtpFechaNacimiento;
+        private TextBox txtApellidoP;
         private Label label3;
-        private TextBox textBox1;
+        private TextBox txtNombreP;
         private Label label2;
         private LinkLabel linkLabel1;
+        private TextBox txtPacienteID;
+        private Label label9;
+        private TextBox txtCorreo;
     }
 }
--- frmPaciente.cs
+++ frmPaciente.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,30 +13,222 @@ namespace ClinicaBD
 {
     public partial class frmPaciente : Form
     {
+        private string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ClinicaBD"].ConnectionString;
         public frmPaciente()
         {
             InitializeComponent();
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             frmOcupaciones frmOcupaciones = new frmOcupaciones();
             frmOcupaciones.ShowDialog();
+        }
+
+        private void frmPaciente_Load(object sender, EventArgs e)
+        {
+            CargarPacientes();
+            CargarOcupaciones();
+        }
+
+        private void CargarOcupaciones()
+        {
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    string query = "SELECT OcupacionID, Nombre FROM Ocupacion";
+                    SqlDataAdapter da = new SqlDataAdapter(query, con);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    cmbOcupacion.DisplayMember = "Nombre";
+                    cmbOcupacion.ValueMember = "OcupacionID";
+                    cmbOcupacion.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar ocupaciones: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnAgregarP_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Validar campos obligatorios
+                if (string.IsNullOrWhiteSpace(txtNombreP.Text) ||
+                    string.IsNullOrWhiteSpace(txtApellidoP.Text) ||
+                    cmbOcupacion.SelectedItem == null ||
+                    string.IsNullOrWhiteSpace(txtCelular.Text) ||
+                    string.IsNullOrWhiteSpace(txtDireccion.Text) || string.IsNullOrWhiteSpace(txtCorreo.Text))
+                {
+                    MessageBox.Show("Todos los campos son obligatorios. Por favor, complete la información.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    string query = "INSERT INTO Paciente (OcupacionID, Nombre, Apellido, Fecha_Nacimiento, Telefono, Correo, Direccion) " +
+                                   "VALUES (@OcupacionID, @Nombre, @Apellido, @FechaNacimiento, @Telefono, @Correo, @Direccion)";
+                    SqlCommand cmd = new SqlCommand(query, con);
+
+                    // Asignar valores desde los controles
+                    cmd.Parameters.AddWithValue("@OcupacionID", (cmbOcupacion.SelectedItem as DataRowView)["OcupacionID"]); //ojoaqui
+                    cmd.Parameters.AddWithValue("@Nombre", txtNombreP.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Apellido", txtApellidoP.Text.Trim());
+                    cmd.Parameters.AddWithValue("@FechaNacimiento", dtpFechaNacimiento.Value);
+                    cmd.Parameters.AddWithValue("@Telefono", txtCelular.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Correo", txtCorreo.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Direccion", txtDireccion.Text.Trim());
+
+                    int rowsAffected = cmd.ExecuteNonQuery(); 
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Paciente agregado con éxito.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        LimpiarCampos(); 
+                        CargarPacientes();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se pudo agregar el paciente. Intente nuevamente.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error al agregar el paciente: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CargarPacientes()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    string query = @"SELECT 
+                                Paciente.PacienteID,
+                                Paciente.Nombre,
+                                Paciente.Apellido,
+                                Paciente.Fecha_Nacimiento,
+                                Paciente.Telefono,
+                                Paciente.Correo,
+                                Paciente.Direccion,
+                                Ocupacion.OcupacionID,
+                                Ocupacion.Nombre AS OcupacionNombre
+                             FROM Paciente
+                             INNER JOIN Ocupacion ON Paciente.OcupacionID = Ocupacion.OcupacionID";
+                    SqlDataAdapter da = new SqlDataAdapter(query, con);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    dgvPacientes.DataSource = dt;
+                    dgvPacientes.Columns["PacienteID"].Visible = false;
+                    dgvPacientes.Columns["OcupacionID"].Visible = false; 
+                    dgvPacientes.Columns["OcupacionNombre"].HeaderText = "Ocupación";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar pacientes: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LimpiarCampos()
+        {
+            txtNombreP.Clear();
+            txtApellidoP.Clear();
+            cmbOcupacion.SelectedIndex = -1;
+            txtCelular.Clear();
+            txtDireccion.Clear();
+            dtpFechaNacimiento.Value = DateTime.Now; 
+            txtPacienteID.Clear();
+            txtCorreo.Clear();
+        }
+
+        private void btnEditarP_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtPacienteID.Text)) 
+                {
+                    MessageBox.Show("Seleccione un paciente para editar.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Mostrar cuadro de confirmación
+                DialogResult result = MessageBox.Show("¿Está seguro de que desea guardar los cambios?", "Confirmación",
+                                                      MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    using (SqlConnection con = new SqlConnection(connectionString))
+                    {
+                        con.Open();
+                        string query = @"UPDATE Paciente 
+                                 SET OcupacionID = @OcupacionID, 
+                                     Nombre = @Nombre, 
+                                     Apellido = @Apellido, 
+                                     Fecha_Nacimiento = @FechaNacimiento, 
+                                     Telefono = @Telefono, 
+                                     Correo = @Correo, 
+                                     Direccion = @Direccion 
+                                 WHERE PacienteID = @PacienteID";
+                        SqlCommand cmd = new SqlCommand(query, con);
+
+                        
+                        cmd.Parameters.AddWithValue("@OcupacionID", (cmbOcupacion.SelectedItem as DataRowView)["OcupacionID"]);
+                        cmd.Parameters.AddWithValue("@Nombre", txtNombreP.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Apellido", txtApellidoP.Text.Trim());
+                        cmd.Parameters.AddWithValue("@FechaNacimiento", dtpFechaNacimiento.Value);
+                        cmd.Parameters.AddWithValue("@Telefono", txtCelular.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Correo", txtCorreo.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Direccion", txtDireccion.Text.Trim());
+                        cmd.Parameters.AddWithValue("@PacienteID", txtPacienteID.Text);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Paciente actualizado con éxito.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            CargarPacientes(); // esto es para refrescar el DataGridView
+                            LimpiarCampos(); // y esto para limpiar controles
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se pudo actualizar el paciente.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al actualizar el paciente: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dgvPacientes_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0) 
+            {
+                DataGridViewRow row = dgvPacientes.Rows[e.RowIndex];
+
+                txtNombreP.Text = row.Cells["Nombre"].Value.ToString();
+                txtApellidoP.Text = row.Cells["Apellido"].Value.ToString();
+                cmbOcupacion.SelectedValue = row.Cells["OcupacionID"].Value; 
+                dtpFechaNacimiento.Value = Convert.ToDateTime(row.Cells["Fecha_Nacimiento"].Value);
+                txtCelular.Text = row.Cells["Telefono"].Value.ToString();
+                txtDireccion.Text = row.Cells["Direccion"].Value.ToString();
+                txtCorreo.Text = row.Cells["Correo"].Value.ToString();
+                txtPacienteID.Text = row.Cells["PacienteID"].Value.ToString(); 
+            }
         }
     }
 }
